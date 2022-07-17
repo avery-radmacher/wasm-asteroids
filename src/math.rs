c1694f9c5b87c9680640add6017ade2d7bb12a8d
avery.radmacher <avery.radmacher@gmail.com>
2022-07-17 10:07:27 -0400

    Use Self in Vec2D impls

--- src/math.rs
+++ src/math.rs
@@ -7,9 +7,9 @@ pub struct Vec2D {
 }
 
 impl Add for Vec2D {
-    type Output = Vec2D;
-    fn add(self, other: Vec2D) -> Vec2D {
-        Vec2D {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self {
             x: self.x + other.x,
             y: self.y + other.y,
         }
@@ -17,9 +17,9 @@ impl Add for Vec2D {
 }
 
 impl Sub for Vec2D {
-    type Output = Vec2D;
-    fn sub(self, other: Vec2D) -> Vec2D {
-        Vec2D {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self {
             x: self.x - other.x,
             y: self.y - other.y,
         }
@@ -27,26 +27,26 @@ impl Sub for Vec2D {
 }
 
 impl AddAssign for Vec2D {
-    fn add_assign(&mut self, other: Vec2D) {
+    fn add_assign(&mut self, other: Self) {
         *self = *self + other;
     }
 }
 
 impl SubAssign for Vec2D {
-    fn sub_assign(&mut self, other: Vec2D) {
+    fn sub_assign(&mut self, other: Self) {
         *self = *self - other
     }
 }
 
 impl Vec2D {
-    pub fn zero() -> Vec2D {
-        Vec2D { x: 0.0, y: 0.0 }
+    pub fn zero() -> Self {
+        Self { x: 0.0, y: 0.0 }
     }
-    pub fn one() -> Vec2D {
-        Vec2D { x: 1.0, y: 0.0 }
+    pub fn one() -> Self {
+        Self { x: 1.0, y: 0.0 }
     }
 
-    pub fn dot(self, other: Vec2D) -> f64 {
+    pub fn dot(self, other: Self) -> f64 {
         self.x * other.x + self.y * other.y
     }
 
@@ -54,31 +54,31 @@ impl Vec2D {
         self.dot(self).sqrt()
     }
 
-    pub fn cross(self, other: Vec2D) -> f64 {
+    pub fn cross(self, other: Self) -> f64 {
         self.x * other.y - self.y * other.x
     }
 
-    pub fn normalize(&self) -> Vec2D {
+    pub fn normalize(&self) -> Self {
         self.scale(1.0 / self.len())
     }
 
-    pub fn rotate(&self, angle: f64) -> Vec2D {
+    pub fn rotate(&self, angle: f64) -> Self {
         let sin = angle.sin();
         let cos = angle.cos();
-        Vec2D {
+        Self {
             x: self.x * cos - self.y * sin,
             y: self.y * cos + self.x * sin,
         }
     }
 
-    pub fn scale(&self, factor: f64) -> Vec2D {
-        Vec2D {
+    pub fn scale(&self, factor: f64) -> Self {
+        Self {
             x: self.x * factor,
             y: self.y * factor,
         }
     }
 
-    pub fn rem_euclid_assign(&mut self, rhs: &Vec2D) {
+    pub fn rem_euclid_assign(&mut self, rhs: &Self) {
         self.x = self.x.rem_euclid(rhs.x);
         self.y = self.y.rem_euclid(rhs.y);
     }
