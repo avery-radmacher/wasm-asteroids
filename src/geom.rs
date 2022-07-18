30159180c9a2c912224c8e8361f594fff9b813b1
avery.radmacher <avery.radmacher@gmail.com>
2022-07-18 08:25:53 -0400

    Fix more comments

--- src/geom.rs
+++ src/geom.rs
@@ -14,7 +14,7 @@ fn closest_triangle_point(p: Vec2D, a: Vec2D, b: Vec2D, c: Vec2D) -> Vec2D {
     // P’ = A + t*AC, t = b_nom / (b_nom + b_denom)
     let b_nom = cp.dot(ca);
     let b_denom = -ap.dot(ca);
-    // Vertex region early out
+    // Vertex region early return
     if c_nom <= 0.0 && b_denom <= 0.0 {
         return a;
     }
@@ -22,14 +22,14 @@ fn closest_triangle_point(p: Vec2D, a: Vec2D, b: Vec2D, c: Vec2D) -> Vec2D {
     // P’ = B + u*BC, u = a_nom / (a_nom + a_denom)
     let a_nom = bp.dot(bc);
     let a_denom = -cp.dot(bc);
-    // Vertex region early outs
+    // Vertex region early returns
     if a_nom <= 0.0 && c_denom <= 0.0 {
         return b;
     }
     if b_nom <= 0.0 && a_denom <= 0.0 {
         return c;
     }
-    // P is outside (or on) AB if the triple scalar product [N PA PB] <= 0
+    // P is outside (or on) AB if the triple scalar product [N AP BP] <= 0
     let n = ab.cross(ca);
     let vc = n * ap.cross(bp);
     // If P outside AB and within feature region of AB,
@@ -37,14 +37,14 @@ fn closest_triangle_point(p: Vec2D, a: Vec2D, b: Vec2D, c: Vec2D) -> Vec2D {
     if vc > 0.0 && c_nom >= 0.0 && c_denom >= 0.0 {
         return a + ab.scale(c_nom / (c_nom + c_denom));
     }
-    // P is outside (or on) BC if the triple scalar product [N PB PC] <= 0
+    // P is outside (or on) BC if the triple scalar product [N BP CP] <= 0
     let va = n * bp.dot(cp);
     // If P outside BC and within feature region of BC,
     // return projection of P onto BC
     if va >= 0.0 && a_nom >= 0.0 && a_denom >= 0.0 {
         return b + bc.scale(a_nom / (a_nom + a_denom));
     }
-    // P is outside (or on) CA if the triple scalar product [N PC PA] <= 0
+    // P is outside (or on) CA if the triple scalar product [N CP AP] <= 0
     let vb = n * cp.cross(ap);
     // If P outside CA and within feature region of CA,
     // return projection of P onto CA
