348a54f67bc283af7b52d9fa502b5ad22d09db36
avery.radmacher <avery.radmacher@gmail.com>
2022-07-18 08:14:13 -0400

    Refactor names

--- src/geom.rs
+++ src/geom.rs
@@ -8,23 +8,23 @@ fn closest_triangle_point(p: Vec2D, a: Vec2D, b: Vec2D, c: Vec2D) -> Vec2D {
     let (ca, cp) = (a - c, p - c);
     // Compute parametric position s for projection P’ of P on AB,
     // P’ = A + s*AB, s = snom/(snom+sdenom)
-    let snom = ap.dot(ab);
-    let sdenom = -bp.dot(ab);
+    let c_nom = ap.dot(ab);
+    let c_denom = -bp.dot(ab);
     // Compute parametric position t for projection P’ of P on AC,
     // P’ = A + t*AC, s = tnom/(tnom+tdenom)
-    let tnom = cp.dot(ca);
-    let tdenom = -ap.dot(ca);
-    if snom <= 0.0 && tdenom <= 0.0 {
+    let b_nom = cp.dot(ca);
+    let b_denom = -ap.dot(ca);
+    if c_nom <= 0.0 && b_denom <= 0.0 {
         return a;
     } // Vertex region early out
       // Compute parametric position u for projection P’ of P on BC,
       // P’ = B + u*BC, u = unom/(unom+udenom)
-    let unom = bp.dot(bc);
-    let udenom = -cp.dot(bc);
-    if unom <= 0.0 && sdenom <= 0.0 {
+    let a_nom = bp.dot(bc);
+    let a_denom = -cp.dot(bc);
+    if a_nom <= 0.0 && c_denom <= 0.0 {
         return b;
     } // Vertex region early out
-    if tnom <= 0.0 && udenom <= 0.0 {
+    if b_nom <= 0.0 && a_denom <= 0.0 {
         return c;
     } // Vertex region early out
       // P is outside (or on) AB if the triple scalar product [N PA PB] <= 0
@@ -32,28 +32,28 @@ fn closest_triangle_point(p: Vec2D, a: Vec2D, b: Vec2D, c: Vec2D) -> Vec2D {
     let vc = n * ap.cross(bp);
     // If P outside AB and within feature region of AB,
     // return projection of P onto AB
-    if vc > 0.0 && snom >= 0.0 && sdenom >= 0.0 {
-        return a + ab.scale(snom / (snom + sdenom));
+    if vc > 0.0 && c_nom >= 0.0 && c_denom >= 0.0 {
+        return a + ab.scale(c_nom / (c_nom + c_denom));
     }
     // P is outside (or on) BC if the triple scalar product [N PB PC] <= 0
     let va = n * bp.dot(cp);
     // If P outside BC and within feature region of BC,
     // return projection of P onto BC
-    if va >= 0.0 && unom >= 0.0 && udenom >= 0.0 {
-        return b + bc.scale(unom / (unom + udenom));
+    if va >= 0.0 && a_nom >= 0.0 && a_denom >= 0.0 {
+        return b + bc.scale(a_nom / (a_nom + a_denom));
     }
     // P is outside (or on) CA if the triple scalar product [N PC PA] <= 0
     let vb = n * cp.cross(ap);
     // If P outside CA and within feature region of CA,
     // return projection of P onto CA
-    if vb >= 0.0 && tnom >= 0.0 && tdenom >= 0.0 {
-        return c + ca.scale(tnom / (tnom + tdenom));
+    if vb >= 0.0 && b_nom >= 0.0 && b_denom >= 0.0 {
+        return c + ca.scale(b_nom / (b_nom + b_denom));
     }
     // P must project inside face region. Compute Q using barycentric coordinates
-    let u = va / (va + vb + vc);
-    let v = vb / (va + vb + vc);
-    let w = 1.0 - u - v; // = vc / (va + vb + vc)
-    return a.scale(u) + b.scale(v) + c.scale(w);
+    let a_scale = va / (va + vb + vc);
+    let b_scale = vb / (va + vb + vc);
+    let c_scale = 1.0 - a_scale - b_scale; // = vc / (va + vb + vc)
+    return a.scale(a_scale) + b.scale(b_scale) + c.scale(c_scale);
 }
 
 pub fn test_circle_triangle(center: Vec2D, radius: f64, a: Vec2D, b: Vec2D, c: Vec2D) -> bool {
