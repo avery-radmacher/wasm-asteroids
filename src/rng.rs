193d953db170abc3d1bfc34708757697b5ba5c76
avery.radmacher <avery.radmacher@gmail.com>
2022-08-08 09:28:02 -0400

    Remove outdated rust glue

--- src/rng.rs
+++ src/rng.rs
@@ -1,12 +1,6 @@
 use rand::SeedableRng;
 pub use rand::{distributions::Standard, rngs::SmallRng, Rng};
-use wasm_bindgen::prelude::*;
 use web_sys::window;
-
-#[wasm_bindgen(module = "/js/demo.js")]
-extern "C" {
-    fn js_fill_rand(buf: &mut [u8]) -> usize;
-}
 
 pub fn new_rng() -> Option<SmallRng> {
     let mut seed = [0u8; 16];
